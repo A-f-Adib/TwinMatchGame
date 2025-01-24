f7e4995bb089321370f11e5a57b5f2ed24d9df74
A.F. Adib <a.f.adib36@gmail.com>
2025-01-25 02:41:54 +0600

    Initial commit

---
+++ TwinMatchGame/TwinMatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinMatchGameApp.swift
+//  TwinMatchGame
+//
+//  Created by A.F. Adib on 25/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TwinMatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
